06e15d6ae44bf52a072e8ed1e2bcc7d07f6a1432
gokulapriyandckap <bgokulapriyandckap@gmail.com>
2023-06-20 14:21:05 +0530

    crud completed after submission

--- controller/userController.php
+++ controller/userController.php
@@ -13,9 +13,6 @@ class userController{
         $allData = $this->userModel->fetch();
         require 'view/list.php';
     }
-//    public function createImage($Files){
-//        $this->userModel->createImage($Files['product_image']);
-//    }
     public function create($data, $image){
         $this->userModel->create($data['product_name'],$data['sku'],$data['price'],$data['brand'],$data['date'],$data['available_stock'],$image['product_image']);
     }
